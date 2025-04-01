aa15f22d7b34873a589b862c269c280e2398d8a1
Rexsxy Bima <Rexsy.Bimq12@gmail.com>
2025-04-01 07:50:26 +0700

    feat(demo): add todos endpoint and template updates

--- app/routes/demo.py
+++ app/routes/demo.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Request
 from . import templates
+from app.models.todo import Todo
 
 
 router = APIRouter(prefix="/demo")
@@ -8,3 +9,15 @@ router = APIRouter(prefix="/demo")
 @router.get("/")
 async def homepage(request: Request):
     return templates.TemplateResponse(request=request, name="index.html")
+
+
+@router.get("/todos")
+async def view_todos(request: Request):
+    todos = [
+        Todo(title="Buy milk"),
+        Todo(title="Buy eggs"),
+        Todo(title="Buy bread"),
+    ]
+    return templates.TemplateResponse(
+        request=request, name="index.html", context={"todos": todos}
+    )
